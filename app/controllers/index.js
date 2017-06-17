efd18652a440a956eda0b4f2187fb61e10e5fc52
Suzanne Brenchley <kem14002@byui.edu>
2017-06-17 14:58:12 -0600

    calculated price

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,4 +8,105 @@ router.get('/', function(req, res) {
   res.render('home')
 })
 
+router.get('/price', function(req, res) {
+  var price = 0;
+  var error = false;
+  var weight = req.query.weight;
+  var letterType = req.query.letterType;
+
+  var prettyLetterType = letterType;
+  switch (letterType) {
+    case "letters_stamped": {
+      prettyLetterType = "Letter (Stamped)";
+      break;
+    }
+    case "letters_metered": {
+      prettyLetterType = "Letter (Metered)";
+      break;
+    }
+    case "parcels": {
+      prettyLetterType = "Parcels"
+      break;
+    }
+    case "large_envelopes": {
+      prettyLetterType = "Large Envelopes";
+      break;
+    }
+
+  }
+
+  if (weight == 3.5) {
+    weight = 3.5;
+  }
+  else {
+    weight = Math.round(req.query.weight);
+  }
+
+
+
+  if(letterType == "large_envelopes") {
+    if (weight >= 2 && weight <= 13) {
+      price = ((weight-1) * 0.21) + 0.98;
+    }
+    else if (weight <= 1) {
+      price = 0.98;
+    }
+    else if (weight > 13) {
+      error = "Large Envelopes cannot contain weight greater than 13 oz";
+    }
+  }
+
+  if(letterType == "letters_stamped") {
+    if (weight >= 2 && weight < 3.5) {
+      price = ((weight-1) * 0.21) + 0.49;
+    }
+    else if (weight <= 1) {
+      price = 0.49;
+    }
+    else if (weight == 3.5) {
+      price = 1.12;
+    }
+    else if (weight > 3.5) {
+      error ="Letters (stamped) cannot contain weight greater than 3.5 oz";
+    }
+  }
+
+  if(letterType == "letters_metered") {
+    if (weight >= 2 && weight < 3.5) {
+      price = ((weight-1) * 0.21) + 0.46;
+    }
+    else if (weight <= 1) {
+      price = 0.46;
+    }
+    else if (weight == 3.5) {
+      price = 1.09;
+    }
+    else if (weight > 3.5) {
+      error = "Letters (metered) cannot contain weight greater than 3.5 oz";
+    }
+  }
+
+  if(letterType == "parcels") {
+    if (weight <= 4) {
+      price = 2.67;
+    }
+    else if (weight >= 5 && weight <= 13 ) {
+      price = ((weight-1) * 0.18) + 2.67;
+    }
+    else if (weight > 13) {
+      error = "Parcels cannot contain weight greater than 13 oz";
+    }
+  }
+
+  res.render('price', { price: price.toFixed(2),
+                        weight: req.query.weight,
+                        letterType: prettyLetterType,
+                        error: error });
+  console.log(weight);
+  console.log(price);
+})
+
 module.exports = router
+
+// error = false otherwise = string
+// price = 0
